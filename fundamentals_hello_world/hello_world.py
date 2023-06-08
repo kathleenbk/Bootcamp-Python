d267f334adc064f02721c285141d1c1352b8afc9
kathleenbk <kavanagh.kathleenb@gmail.com>
2023-06-07 17:32:00 -0700

    Clean up repos - Fundamentals: Hello World

---
+++ fundamentals_hello_world/hello_world.py
@@ -0,0 +1,17 @@
+# 1. 
+print("Hello World")
+# 2. 
+name = "Katie"
+print("Hello", name,"!")	# with a comma
+print("Hello " + name + "!")	# with a +
+# 3.
+number = 33
+print("Hello", number, "!")	# with a comma
+print(f"Hello {number}!")
+print("Hello " + str(33) + "!")
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1,fave_food2)) # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
